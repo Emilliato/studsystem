967fb645a14b7a4ef7408eb1fd66ab90a4343fec
Emillio Itai Marambo <emilliomarambo@gmail.com>
2020-04-28 20:47:47 +0200

    make it better

--- studsystem/settings.py
+++ studsystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5wi%78gm#c4()51n7_vtfppidbnl%ced^y*oajc-x(8z@zffb3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studmanage.herokuapp.com']
 
 APPEND_SLASH=False
 # Application definition
