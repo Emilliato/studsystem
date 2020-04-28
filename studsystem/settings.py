7f75dd2a1471a19b7c7de8fad3f4fb3c7a8d5ab7
Emillio Itai Marambo <emilliomarambo@gmail.com>
2020-04-28 21:57:08 +0200

    Update Settings

--- studsystem/settings.py
+++ studsystem/settings.py
@@ -83,9 +83,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'studsystem.wsgi.application'
 
-CORS_ORIGIN_WHITELIST = [
-    "*"
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
